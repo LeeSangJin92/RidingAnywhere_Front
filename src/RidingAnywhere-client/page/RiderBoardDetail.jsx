8f0bd39d57122cc414a381130ac2e3d9adc4cdcc
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-11-05 16:30:03 +0900

    2024, 11. 05 🛠️ 라이더 보드 디테일 페이지 CSS 작업 🚧
    🛠️ 라이더 보드 디테일 페이지 반응형 CSS 작업 🚧
    🛠️ 라이더 보드 리스트 페이지 반응형 작업 완료 ✅

--- src/RidingAnywhere-client/page/RiderBoardDetail.jsx
+++ src/RidingAnywhere-client/page/RiderBoardDetail.jsx
@@ -207,7 +207,7 @@ const RiderBoardDetail = () => {
     }
 
     return (
-        <main>
+        <main className='Main_RiderBoardDetail'>
              <DefaultHeader/>
             <section className='RiderBoardDetail'>
                 <div className='PageTopLine'>
